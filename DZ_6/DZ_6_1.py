a3bcc1392a49a758b600d4897e7e4975b8681045
Lem-49 <Lem0049@gmail.com>
2021-08-05 22:49:03 +0300

    third

--- DZ_6/DZ_6_1.py
+++ DZ_6/DZ_6_1.py
@@ -1,13 +1,32 @@
-import collections
+shows = {
+    'Секретные материалы': {'Жанр': 'фантастика', 'Рейтинг': 0.9},
+    'Ведьмак': {'Жанр': 'фэнтази', 'Рейтинг': 0.95},
+    'Клан Сопрано': {'Жанр': 'криминал', 'Рейтинг': '0.8'},
+    '24': {'Genre': 'драма', 'Rating': 0.75},
+    'Черное зеркало': {'Жанр': 'фантастика', 'Рейтинг': 0.98},
+    'Во все тяжкие': {'Жанр': 'криминал', 'Рейтинг': '0.85'},
+    'Игра престолов': {'Жанр': 'фэнтази', 'Рейтинг': 0.87},
+    'Карточный домик': {'Genre': 'драма', 'Rating': 0.82},
+    'Рик и Морти': {'Жанр': 'фантастика', 'Рейтинг': 1}
+}
 
-shows = {'Секретные материалы': {'Жанр': 'фантастика', 'Рейтинг': 0.9}}
+average = []
+for i in shows.keys():
+    try:
+        if (shows[i]['Рейтинг']) > 0:
+            average.append(float(shows[i]['Рейтинг']))
+    except KeyError as k_e:
+        print(f'KeyError    : {k_e}')
+    except TypeError as t_e:
+        print(f'TypeError   : {t_e}')
+    try:
+        if (shows[i]['Rating']) > 0:
+            average.append(float(shows[i]['Rating']))
+    except KeyError as k_e:
+        print(f'KeyError    : {k_e}')
+    except TypeError as t_e:
+        print(f'TypeError   : {t_e}')
 
-s = []
-
-for key, value in shows.items():
-    s.append(value)
-
-
-print(s)
-
+a = float(sum(average) / len(average))
+print(a)
 
