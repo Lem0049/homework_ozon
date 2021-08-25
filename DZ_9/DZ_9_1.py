9d289a06fbef0cc77b07e9590d8cf924eed7adaf
Lem-49 <Lem0049@gmail.com>
2021-08-25 23:50:16 +0300

    five

--- DZ_9/DZ_9_1.py
+++ DZ_9/DZ_9_1.py
@@ -1,2 +1,16 @@
-with open('lesson09_closest_galaxies.csv', encoding='utf8') as i:
-    print(i.read())
+import re
+
+z = []
+with open('lesson09_closest_galaxies.csv', 'r', encoding='utf8') as i:
+    s = i.readline()
+    for s in i:
+        z.append(re.split('[,;\n]', s))
+        if re.search(r'Кит|Пегас|Рыбы', s):
+            print(s.split(',')[0])
+        if re.search(r'^[a-z,A-Z]', s):
+            print(s.split(',')[0])
+        if re.search(r'\d$', s.split(',')[0]):
+            print(s.split(',')[0])
+
+
+print(z)
