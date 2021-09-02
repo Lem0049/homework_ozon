08a9deb0b3d8d03417280b55dce56627ef23c1ff
Lem-49 <Lem0049@gmail.com>
2021-09-02 23:38:41 +0300

    six

--- DZ_9/DZ_9_3.py
+++ DZ_9/DZ_9_3.py
@@ -1,10 +1,10 @@
 import re
 
 with open('cats of ultrah.txt', 'r', encoding='utf8') as file:
-    z = []
+    cat_count = []
     for row in file.read().splitlines():
         result = re.findall(r"[Кк]ош\w{1,3}\b", row)
         if len(result) > 0:
-            z = z + result
+            cat_count = cat_count + result
 
-print(len(z))
+print(len(cat_count))
--- DZ_9/DZ_9_4.py
+++ DZ_9/DZ_9_4.py
@@ -5,6 +5,11 @@ import re
 req = requests.get('https://ru.wikipedia.org/wiki/Небьюла')
 result = req.text
 soup = BeautifulSoup(result, 'html.parser')
-tet = soup.find_all('a', href=True)
-item_list = [i.get('href', http=True) for i in tet]
+tet = soup.find_all('a', href=re.compile(r'^http'))
+wiki = soup.find_all('a', href=re.compile(r'https?://\w+.wiki\w+'))
+item_list = set([i.get('href') for i in tet])
+wiki_list = set([z.get('href') for z in wiki])
+final_list = item_list - wiki_list      # item_list.difference(wiki_list)
+print(wiki_list)
 print(item_list)
+print(final_list)
